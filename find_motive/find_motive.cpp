57f7e2cf38ab9cbbf20197e675b07a0ce59fef0f
aboubekre.sayoud <aboubekre.sayoud@epita.fr>
2022-09-23 10:23:49 +0200

    exo terminé

--- find_motive/find_motive.cpp
+++ find_motive/find_motive.cpp
@@ -8,8 +8,7 @@ int main(int argc , char** argv){
     std::fstream newFile;
     newFile.open(argv[1],std::ios::in);
 
-
-    if(newFile.is_open()){
+    if(newFile.is_open() && argc==3){
         int cpt = 0;
         std::string line;
         std::string motive = argv[2];
@@ -30,7 +29,8 @@ int main(int argc , char** argv){
 
         }
         std::cout << "The file "<<argv[1]<<" contains "<<cpt <<" words containing the motive "<< argv[2]<<"\n";
-        return cpt;
+        return 0;
     }
 std::cout<<"The file "<<argv[1]<<"could not be opened.\n";
+return 1;
 }
