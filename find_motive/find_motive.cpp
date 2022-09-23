c8001d94df835eeb42e94cc8dba7a58e1cc034d2
aboubekre.sayoud <aboubekre.sayoud@epita.fr>
2022-09-23 10:10:28 +0200

    Finished exo

---
+++ find_motive/find_motive.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main(int argc , char** argv){
+    
+    std::fstream newFile;
+    newFile.open(argv[1],std::ios::in);
+
+
+    if(newFile.is_open()){
+        int cpt = 0;
+        std::string line;
+        std::string motive = argv[2];
+
+        while(getline(newFile,line)){
+            size_t pos = 0;
+            std::string word ;
+
+            while( (pos=line.find(" "))!= std::string::npos ){
+                word = line.substr(0,pos);
+
+                if(word.find(argv[2])!=std::string::npos){
+                    cpt+=1;
+                }
+                line.erase(0,pos+1);// Pos + delimiter length
+            }
+
+
+        }
+        std::cout << "The file "<<argv[1]<<" contains "<<cpt <<" words containing the motive "<< argv[2]<<"\n";
+        return cpt;
+    }
+std::cout<<"The file "<<argv[1]<<"could not be opened.\n";
+}
