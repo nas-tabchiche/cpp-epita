7c9712a6bc64551e6be961f8ddd6a06336168807
aboubekre.sayoud <aboubekre.sayoud@epita.fr>
2022-09-28 13:41:27 +0200

    changed fct

--- find_motive/main.cpp
+++ find_motive/main.cpp
@@ -11,20 +11,26 @@ int main(int argc , char** argv){
     if(newFile.is_open() && argc==3){
         int cpt = 0;
         std::string line;
+        
         std::string motive = argv[2];
 
         while(getline(newFile,line)){
             size_t pos = 0;
             std::string word ;
-
+            
             while( (pos=line.find(" "))!= std::string::npos ){
                 word = line.substr(0,pos);
-
                 if(word.find(argv[2])!=std::string::npos){
                     cpt+=1;
                 }
                 line.erase(0,pos+1);// Pos + delimiter length
             }
+            word = line.substr(0,pos);
+            std::cout << word << cpt;
+            if(word.find(argv[2])!=std::string::npos){
+                cpt+=1;
+            }
+            line.erase(0,pos+1);// Pos + delimiter length
 
 
         }
