58824595e211440c42e83422e1312b422facc645
aboub <aboubekre.sayoud@gmail.com>
2022-10-04 18:34:50 +0200

    working find motive

--- find_motive/main.cpp
+++ find_motive/main.cpp
@@ -1,42 +1,46 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+#include<string>
+#include<sstream>
+#include<fstream>
+#include<iostream>
 
+int main(int argc, char **argv)
+{
 
-int main(int argc , char** argv){
-    
-    std::fstream newFile;
-    newFile.open(argv[1],std::ios::in);
-
-    if(newFile.is_open() && argc==3){
-        int cpt = 0;
-        std::string line;
-        
-        std::string motive = argv[2];
-
-        while(getline(newFile,line)){
-            size_t pos = 0;
-            std::string word ;
-            
-            while( (pos=line.find(" "))!= std::string::npos ){
-                word = line.substr(0,pos);
-                if(word.find(argv[2])!=std::string::npos){
-                    cpt+=1;
-                }
-                line.erase(0,pos+1);// Pos + delimiter length
-            }
-            word = line.substr(0,pos);
-            std::cout << word << cpt;
-            if(word.find(argv[2])!=std::string::npos){
-                cpt+=1;
-            }
-            line.erase(0,pos+1);// Pos + delimiter length
-
-
-        }
-        std::cout << "The file "<<argv[1]<<" contains "<<cpt <<" words containing the motive "<< argv[2]<<"\n";
-        return 0;
+    std::ifstream file(argv[1]);
+    if (file.fail()) 
+    {
+        std::cout << "The file " << argv[1] << " could not be opened." << std::endl;
+        return 1;
     }
-std::cout<<"The file "<<argv[1]<<" could not be opened.\n";
-return 1;
+
+    std::string motive(argv[2]);
+    std::string line;
+
+    size_t count = 0;
+
+    while (std::getline(file, line))
+    {
+        if (line == "")
+        {
+            continue;
+        }
+        size_t pos = 0;
+        std::string match;
+        while (((pos = line.find(' ')) != std::string::npos)) 
+        {
+            match = line.substr(0, pos);
+            if (match.find(motive) != std::string::npos) 
+            {
+                count++;
+            }
+            line.erase(0, pos + 1);
+        }
+
+        if (line != "" && (line.find(motive) != std::string::npos))
+        {
+            count++;
+        }
+    }
+    std::cout << "The file " << argv[1] << " contains " << count << " words containing the motive " << motive << std::endl;
+    return 0;
 }
