aaa27fd68099580f534dbba3dff60d3ee195caa0
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-10-27 15:31:09 +0200

    Fix find_motive

--- find_motive/main.cpp
+++ find_motive/main.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <fstream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    std::ifstream file(argv[1]);
-    if (file.fail())
-    {
-        std::cout << "The file " << argv[1] << " could not be opened."
-            << std::endl;
+
+    ifstream file;
+    file.open (argv[1]);
+    if (!file) {
+        cout << "The file " << argv[1] << " could not be opened.";
         return 1;
     }
-    int count = 0;
-    std::string word;
-    while (file >> word)
-        if (word.find(argv[2]) != std::string::npos)
-            count++;
+
+    int occ = 0;
+    string word;
+    while (file >> word) {
+        int i = 0;
+        if (word.find(argv[2]) != std::string::npos) {
+            occ++;
+        }
+    }
+
+    cout << "The file " << argv[1] << " contains "<< occ << " words containing the motive "<< argv[2];
+
     file.close();
-    std::cout << "The file " << argv[1] << " contains " << count << " words containing the motive " << argv[2] << "." << std::endl;
     return 0;
 }
