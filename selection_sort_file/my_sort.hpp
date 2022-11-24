37495e801e2956d999996e74d1007aab2f31f2da
aboub <aboubekre.sayoud@gmail.com>
2022-11-24 16:39:11 +0100

    fixed

--- selection_sort_file/my_sort.hpp
+++ selection_sort_file/my_sort.hpp
@@ -1,20 +1,29 @@
 // Change me instead
 // All occurences of XXX need to be replaced with
 // something more meaningful
+template<class TYPE>
+void swap(TYPE& a, TYPE& b) {
+    TYPE tmp = a;
+    a = b;
+    b = tmp;
+}
 
-
-template<typename T>
-void my_selection_sort( T begin, T end) {
-    for (T i = begin; i != end; ++i) {
-        T min = i;
-        for (T j = i; j != end; ++j) {
-            if (*j < *min) {
+template<class IT>
+void my_selection_sort( IT begin, IT end)
+{
+    IT min;
+    IT next;
+    for (IT i = begin; i != end; ++i)
+    {
+        min = i;
+        next = i; 
+        for (IT j = ++next; j != end; ++j)
+        {
+            if (*j < *min)
+            {
                 min = j;
             }
         }
-        // swap
-        auto tmp = *i;
-        *i = *min;
-        *min = tmp;
+        swap(*min, *i);
     }
 }
--- selection_sort_file/sort_file.hpp
+++ selection_sort_file/sort_file.hpp
@@ -1,7 +1,7 @@
 // Do not change
 #include <iostream>
 #include <fstream>
-
+#include <string>
 
 #include <list>
 #include <vector>
@@ -14,115 +14,97 @@
 // something more meaningful
 
 
-template<class V, class C>
-void read_file_to_cont(std::ifstream& f, C& c) {
-    V v;
-    while (f >> v) {
-        c.push_back(v);
-    }
+template<class TYPE, class CONTAINER>
+void read_file_to_cont(std::ifstream& f, CONTAINER& c) {
+  TYPE elm;
+  while (f >> elm) {
+    c.push_back(elm);
+  }
+
 }
 
-template<class V, class C>
+
+template<class TYPE ,class CONTAINER>
 void read_and_sort(std::ifstream& fi, std::ofstream& fo) {
-    C c = {};
-    read_file_to_cont<V, C>(fi, c);
-    my_selection_sort(c.begin(), c.end());
-    for (auto &v : c) {
-        fo << v << std::endl;
-    }
+
+  CONTAINER container = {};
+  read_file_to_cont<TYPE, CONTAINER>(fi, container);
+  my_selection_sort(container.begin(), container.end());
+
+  for (TYPE& elm : container) {
+    fo << elm << "\n";
+  }
+
 }
 
-template<class V>
+
+template<class TYPE>
 void read_and_sort_decide_container(std::ifstream& fi, std::ofstream& fo) {
-  // read second line to decide container
-  char container_type;
-  fi >> container_type;
-  fo << container_type << std::endl;
-  switch (container_type) {
+  char line;
+  fi >> line;
+
+  fo << line << "\n";
+
+  switch(line)
+  {
     case 'l':
-      read_and_sort<V, std::list<V>>(fi, fo);
+      read_and_sort<TYPE, std::list<TYPE>>(fi, fo);
       break;
+
     case 'v':
-      read_and_sort<V, std::vector<V>>(fi, fo);
+      read_and_sort<TYPE, std::vector<TYPE>>(fi, fo);
       break;
+
     case 'd':
-      read_and_sort<V, std::deque<V>>(fi, fo);
+      read_and_sort<TYPE, std::deque<TYPE>>(fi, fo);
       break;
+
     default:
-      throw std::runtime_error("Unknown container");
+      std::runtime_error("Type container doesn't match");
+      break;
   }
 }
 
 
 void read_and_sort_decide_valuetype(std::ifstream& fi, std::ofstream& fo) {
-  // read first line to decide value type
-  char value_type;
-  fi >> value_type;
-  fo << value_type << std::endl;
-  switch(value_type) {
+  char line;
+
+  fi >> line;
+
+  fo << line << "\n";
+  
+  switch(line)
+  {
     case 'i':
       read_and_sort_decide_container<int>(fi, fo);
+      
       break;
+
     case 'u':
       read_and_sort_decide_container<unsigned>(fi, fo);
       break;
+
     case 'f':
       read_and_sort_decide_container<float>(fi, fo);
       break;
+
     case 'd':
       read_and_sort_decide_container<double>(fi, fo);
       break;
+
     case 'c':
       read_and_sort_decide_container<char>(fi, fo);
       break;
+
     case 's':
       read_and_sort_decide_container<std::string>(fi, fo);
       break;
+
     default:
-      throw std::runtime_error("Unknown value type");
+      std::runtime_error("Type value doesn't match");
+      break;
   }
 }
 
-// switch(line[0]) {
-//     case 'i':
-//       template<int>
-//       read_and_sort_decide_container(fi, fo);
-//       break;
-//     case 'u':
-//       template<unsigned int>
-//       read_and_sort_decide_container(fi, fo);
-//       break;
-//     case 'f':
-//       template<float>
-//       read_and_sort_decide_container(fi, fo);
-//       break;
-//     case 'd':
-//       template<double>
-//       read_and_sort_decide_container(fi, fo);
-//       break;
-//     case 'c':
-//       template<char>
-//       read_and_sort_decide_container(fi, fo);
-//       break;
-//     case 's':
-//       template<std::string>
-//       read_and_sort_decide_container(fi, fo);
-//       break;
-//     default:
-//       throw std::runtime_error("Unknown value type");
-//   }
 
-// std::getline(fi, line);
-//   switch(line[0]) {
-//     case 'i':
-//       read_and_sort_decide_container<int>(fi, fo);
-//       break;
-//     case 'd':
-//       read_and_sort_decide_container<double>(fi, fo);
-//       break;
-//     case 's':
-//       read_and_sort_decide_container<std::string>(fi, fo);
-//       break;
-//     default:
-//       throw std::runtime_error("Unknown value type");
-//   }
+
